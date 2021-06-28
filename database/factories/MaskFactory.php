b85b6f6fed93fc391623ef3fcab214aab1235240
Uki7991 <tilek.kubanov@gmail.com>
2021-06-28 08:28:45 +0600

    Initial commit

---
+++ database/factories/MaskFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Mask;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class MaskFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Mask::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
